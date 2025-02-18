6fff3103db7511bd68470ae2f1e9bbd157e59aa6
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-18 10:17:16 +0500

    imp

--- pages/home/home.js
+++ pages/home/home.js
@@ -21,15 +21,15 @@ document.addEventListener('DOMContentLoaded', () => {
       name: "Lauryn Hackett",
       price: 7187024,
       images: [
-        "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="
-      ]
+        "https://www.chiltons.com/cdn/shop/products/Table_Shaker_Heirloom_85053.jpg?v=1634308239"
+      ],
     },
     {
       id: 2,
       name: "Dr Frieda",
       price: 3973847,
       images: [
-        "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="
+        "https://woodsala.com/cdn/shop/products/SGHWS0773_4.jpg?v=1613061618"
       ]
     },
     {
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
       name: "Brenna Kutch",
       price: 7247928,
       images: [
-        "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="
+        "https://homestyledepot.com.ph/wp-content/uploads/2019/11/Toya-Icons-Sophia-4-seater.jpg"
       ]
     },
     {
@@ -45,9 +45,49 @@ document.addEventListener('DOMContentLoaded', () => {
       name: "Archibald",
       price: 8985507,
       images: [
-        "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="
+        "https://homemakersfurniture.scene7.com/is/image/HomemakersFurniture/PABW745703_D?&wid=622&hei=490&fit=fit,1"
       ]
-    }
+    },
+    {
+      id: 5,
+      name: "Archibald",
+      price: 8985507,
+      images: [
+        "https://images-cdn.ubuy.co.in/66802aaaf3a57a0c564a6b5f-79-39-39-modern-dining-table-set-for.jpg"
+      ]
+    },
+    {
+      id: 6,
+      name: "Archibald",
+      price: 8985507,
+      images: [
+        "https://www.orangetree.in/cdn/shop/files/LinnCoffeeTableLifestyle4.jpg?v=1722850287"
+      ]
+    },
+    {
+      id: 7,
+      name: "Archibald",
+      price: 8985507,
+      images: [
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Writing_table_%28bureau_plat%29_MET_DP105403.jpg/275px-Writing_table_%28bureau_plat%29_MET_DP105403.jpg"
+      ]
+    },
+    {
+      id: 8,
+      name: "Archibald",
+      price: 8985507,
+      images: [
+        "https://ventofurniture.com/wp-content/uploads/2024/01/Legno-Center-Table.webp"
+      ]
+    },
+    {
+      id: 9,
+      name: "Archibald",
+      price: 8985507,
+      images: [
+        "https://m.media-amazon.com/images/I/71ot0H1YEpL._AC_UF1000,1000_QL80_.jpg"
+      ]
+    },
   ];
 
   renderFurnitureList(dummyData);

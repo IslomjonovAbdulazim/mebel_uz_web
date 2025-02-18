e50ddbadfe8b3deee18610f72980bb7f3bd9e7d4
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-18 10:23:51 +0500

    imp

--- pages/home/home.js
+++ pages/home/home.js
@@ -18,72 +18,72 @@ document.addEventListener('DOMContentLoaded', () => {
   const dummyData = [
     {
       id: 1,
-      name: "Lauryn Hackett",
-      price: 7187024,
+      name: "Ошхона столи (Обеденный стол)",
+      price: 7_200_000,
       images: [
         "https://www.chiltons.com/cdn/shop/products/Table_Shaker_Heirloom_85053.jpg?v=1634308239"
       ],
     },
     {
       id: 2,
-      name: "Dr Frieda",
-      price: 3973847,
+      name: "Ётокхона тумбочкаси (Прикроватная тумбочка)",
+      price: 3_950_000,
       images: [
         "https://woodsala.com/cdn/shop/products/SGHWS0773_4.jpg?v=1613061618"
       ]
     },
     {
       id: 3,
-      name: "Brenna Kutch",
-      price: 7247928,
+      name: "Оилавий овқат столи (Семейный обеденный стол)",
+      price: 8_250_000,
       images: [
         "https://homestyledepot.com.ph/wp-content/uploads/2019/11/Toya-Icons-Sophia-4-seater.jpg"
       ]
     },
     {
       id: 4,
-      name: "Archibald",
-      price: 8985507,
+      name: "Китоб жавони (Книжный шкаф)",
+      price: 6_100_000,
       images: [
         "https://homemakersfurniture.scene7.com/is/image/HomemakersFurniture/PABW745703_D?&wid=622&hei=490&fit=fit,1"
       ]
     },
     {
       id: 5,
-      name: "Archibald",
-      price: 8985507,
+      name: "Меҳмонхона журнали столи (Журнальный столик)",
+      price: 5_750_000,
       images: [
         "https://images-cdn.ubuy.co.in/66802aaaf3a57a0c564a6b5f-79-39-39-modern-dining-table-set-for.jpg"
       ]
     },
     {
       id: 6,
-      name: "Archibald",
-      price: 8985507,
+      name: "Кофе столи (Кофейный столик)",
+      price: 4_350_000,
       images: [
         "https://www.orangetree.in/cdn/shop/files/LinnCoffeeTableLifestyle4.jpg?v=1722850287"
       ]
     },
     {
       id: 7,
-      name: "Archibald",
-      price: 8985507,
+      name: "Ёзиш столи (Письменный стол)",
+      price: 7_800_000,
       images: [
         "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Writing_table_%28bureau_plat%29_MET_DP105403.jpg/275px-Writing_table_%28bureau_plat%29_MET_DP105403.jpg"
       ]
     },
     {
       id: 8,
-      name: "Archibald",
-      price: 8985507,
+      name: "Ойна таги столи (Туалетный столик)",
+      price: 6_500_000,
       images: [
         "https://ventofurniture.com/wp-content/uploads/2024/01/Legno-Center-Table.webp"
       ]
     },
     {
       id: 9,
-      name: "Archibald",
-      price: 8985507,
+      name: "Диван столи (Столик для дивана)",
+      price: 5_250_000,
       images: [
         "https://m.media-amazon.com/images/I/71ot0H1YEpL._AC_UF1000,1000_QL80_.jpg"
       ]
